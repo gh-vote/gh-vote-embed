6ee903f4530c86bb810c16e7a07ca594f84698ac
ivanjermakov <ivanjermakov1@gmail.com>
2022-02-24 23:22:46 +0100

    Rename project

--- build-script.js
+++ build-script.js
@@ -3,8 +3,8 @@ const concat = require('concat');
 
 (async () => {
   const files = [
-    './dist/gh-vote/main.js',
-    './dist/gh-vote/polyfills.js'
+    './dist/gh-vote-embed/main.js',
+    './dist/gh-vote-embed/polyfills.js'
   ]
   await concat(files, 'dist/gh-vote.js')
 })()
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/gh-vote'),
+      dir: require('path').join(__dirname, './coverage/gh-vote-embed'),
       subdir: '.',
       reporters: [
         {type: 'html'},
